5e04f9d258063952d6a887e455103bc7c683ecc3
Pebody-h <heiderl753@gmail.com>
2024-01-20 16:24:01 -0500

    [Fix]models and database

--- internal/database/database.go
+++ internal/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/Pebody-h/Anime-Api/models"
 	"github.com/Pebody-h/Anime-Api/settings"
@@ -12,13 +13,17 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
+	p := settings.Settings("DB_PORT")
+	dbPort, err := strconv.ParseUint(p, 10, 32)
+	if err != nil {
+		panic("Faild to parse database port")
+	}
 	dbHost := settings.Settings("DB_HOST")
-	dbPort := settings.Settings("DB_PORT")
 	dbUser := settings.Settings("DB_USER")
 	dbPassword := settings.Settings("DB_PASSWORD")
 	dbName := settings.Settings("DB_NAME")
 
-	dsn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, dbPort, dbUser, dbName, dbPassword)
+	dsn := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable password=%s", dbHost, dbPort, dbUser, dbName, dbPassword)
 
 	DB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
--- models/models.go
+++ models/models.go
@@ -4,12 +4,12 @@ import "gorm.io/gorm"
 
 type Animes struct {
 	gorm.Model
-	Title    string  `json:"title"`
-	Image    string  `json:"image"`
-	Synopsis string  `json:"synopsis"`
-	Trailer  string  `json:"trailer"`
-	Episodes int     `json:"episodes"`
-	Score    float32 `json:"score"`
-	Status   string  `json:"status"`
-	Year     int     `json:"year"`
+	Title    string  `json:"title" gorm:"not null"`
+	Image    string  `json:"image" gorm:"not null"`
+	Synopsis string  `json:"synopsis" gorm:"not null"`
+	Trailer  string  `json:"trailer" gorm:"not null"`
+	Episodes int     `json:"episodes" gorm:"not null"`
+	Score    float32 `json:"score" gorm:"not null"`
+	Status   string  `json:"status" gorm:"not null"`
+	Year     int     `json:"year" gorm:"not null"`
 }
